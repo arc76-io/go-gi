e4148af5bccad1ef280658129487ed21767cc3cd
Koen Van Rulo <koen.vr@pm.me>
2021-11-29 22:50:56 +0100

    update dependencies
    minor clean up

--- examples/gradient-radial.go
+++ examples/gradient-radial.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-	dc := magi.NewContext(400, 200)
+	dc := magi.NewContext(200, 200)
 
-	grad := magi.NewRadialGradient(100, 100, 10, 100, 120, 80)
+	grad := magi.NewRadialGradient(100, 100, 10, 100, 100, 80)
 	grad.AddColorStop(0, color.RGBA{0, 255, 0, 255})
 	grad.AddColorStop(1, color.RGBA{0, 0, 255, 255})
 
@@ -20,7 +20,7 @@ func main() {
 	dc.SetColor(color.White)
 	dc.DrawCircle(100, 100, 10)
 	dc.Stroke()
-	dc.DrawCircle(100, 120, 80)
+	dc.DrawCircle(100, 100, 80)
 	dc.Stroke()
 
 	dc.SavePNG("out.png")
--- go.mod
+++ go.mod
@@ -6,3 +6,10 @@ require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
 )
+
+require (
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+)
